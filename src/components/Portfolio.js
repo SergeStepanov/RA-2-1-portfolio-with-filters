7103d878efca4d69791c4b0fa5e9bf38f90ab2d2
Sergey Stepanov <ser1653285@yandex.ru>
2022-04-05 03:05:27 +0300

    ыефке

---
+++ src/components/Portfolio.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+// import PropTypes from 'prop-types'
+
+export default class Portfolio extends Component {
+  // static propTypes =
+  constructor(props) {
+    super(props);
+    this.state = null;
+  }
+
+  render() {
+    return (
+      <div>Portfolio</div>
+    )
+  }
+}
